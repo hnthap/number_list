347332f2346a82c76abba7a11ee0039cb97af5ea
hnthap <94937751+hnthap@users.noreply.github.com>
2024-05-18 14:02:28 +0700

    add NodeJS test file

---
+++ tests/LinkedList.test.js
@@ -0,0 +1,47 @@
+import { deepStrictEqual } from "node:assert/strict";
+import { describe, it } from "node:test";
+import { LinkedList } from "../src/LinkedList.js";
+
+describe("linked list", () => {
+  it("new list must be empty", () => {
+    const list = new LinkedList();
+
+    deepStrictEqual(list.size, 0);
+    deepStrictEqual(list.head, null);
+    deepStrictEqual(list.tail, null);
+  });
+
+  it("add tail", () => {
+    const list = new LinkedList();
+    let length = 1;
+    for (const value of [12, "Hello, World!", true]) {
+      list.addTail(value);
+      
+      deepStrictEqual(list.size, length);
+      deepStrictEqual(list.tail.value, value);
+      length += 1;
+    }
+  });
+
+  it("get by index", () => {
+    const values = [100, "Earth", false, true, "🥶🥶🥶"];
+    const list = LinkedList.fromArray(values);
+
+    deepStrictEqual(list.size, values.length);
+    for (let i = 0; i < values.length; i += 1) {
+      deepStrictEqual(list.get(i), values[i]);
+    }
+  });
+
+  it("from/to Array", () => {
+    const array = [100, "Earth", false, true, "🥶🥶🥶"];
+    const list = LinkedList.fromArray(array);
+    const anotherArray = list.toArray();
+    const anotherList = LinkedList.fromArray(anotherArray);
+    deepStrictEqual(array.length, anotherArray.length);
+    for (let i = 0; i < array.length; i += 1) {
+      deepStrictEqual(array[i], anotherArray[i])
+    }
+    deepStrictEqual(LinkedList.isEquals(list, anotherList), true);
+  })
+});

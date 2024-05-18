4a1e22acff06bdf64081e5a7954141935ff17d72
hnthap <94937751+hnthap@users.noreply.github.com>
2024-05-18 18:10:53 +0700

    run test "get first n prime numbers" with some different n values

--- tests/util.test.js
+++ tests/util.test.js
@@ -3,17 +3,19 @@ import { describe, it } from "node:test";
 import { doesDirectoryExist, getFirstNPrimeNumbers } from "../src/util.js";
 
 describe("utility methods", () => {
-  it("get first thousand prime numbers", () => {
-    getFirstNPrimeNumbers(1_000).forEach((value) => {
-      if (value === 2) return;
-      if (value === 3) return;
-      notDeepStrictEqual(value % 2, 0);
-      notDeepStrictEqual(value % 3, 0);
-      for (let i = 5; i * i <= value; i += 6) {
-        notDeepStrictEqual(value % i, 0);
-        notDeepStrictEqual(value % (i + 2), 0);
-      }
-    });
+  it("get first n prime numbers", () => {
+    for (const n of [1_000, 799, 203]) {
+      getFirstNPrimeNumbers(n).forEach((value) => {
+        if (value === 2) return;
+        if (value === 3) return;
+        notDeepStrictEqual(value % 2, 0);
+        notDeepStrictEqual(value % 3, 0);
+        for (let i = 5; i * i <= value; i += 6) {
+          notDeepStrictEqual(value % i, 0);
+          notDeepStrictEqual(value % (i + 2), 0);
+        }
+      });
+    }
   });
 
   it("the directory src exists but <empty> does not", async () => {

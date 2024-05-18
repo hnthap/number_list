920d35bbc88cfb0250c45312db2700000975b830
hnthap <94937751+hnthap@users.noreply.github.com>
2024-05-18 17:07:34 +0700

    move the code to get the first n prime numbers from index.js to util.js

--- src/util.js
+++ src/util.js
@@ -1,5 +1,35 @@
 import { access, mkdir } from "node:fs/promises";
 import { constants as fsPromisesConstants } from "node:fs/promises";
+import LinkedList from "./LinkedList.js";
+
+/**
+ * @param {number} n 
+ * @returns {LinkedList}
+ */
+export function getFirstNPrimeNumbers(n) {
+  const list = new LinkedList();
+  if (typeof n !== "number" || n < 0 || !Number.isInteger(n)) {
+    return list;
+  }
+  if (n > 0) list.addTail(2);
+  if (n > 1) list.addTail(3);
+  for (let i = 6; list.size() !== n; i += 6) {
+    for (let j = -1; j != 3; j += 2) {
+      let isPrime = true;
+      const v = i + j;
+      list.forEach((value, _, stopHere) => {
+        if (v % value === 0 && value * value <= v) {
+          isPrime = false;
+          stopHere();
+        }
+      });
+      if (isPrime) {
+        list.addTail(v);
+      }
+    }
+  }
+  return list;
+}
 
 /**
  *
